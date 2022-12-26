b648de15e7458985cb27f07609f6a2511b8095b8
Justim Odendaal <justim.odendaal@gmail.com>
2022-12-26 22:02:23 +0000

    Added Integration Test for List prompt.

--- src/OpenAI.Net.Integration.Tests/TextCompletionService_Get.cs
+++ src/OpenAI.Net.Integration.Tests/TextCompletionService_Get.cs
@@ -24,7 +24,8 @@ namespace OpenAI.Net.Integration.Tests
             Assert.That(response.Result?.Choices?.Count() == 1, Is.EqualTo(isSuccess), "Choices are not mapped correctly");
             if (echo.HasValue && isSuccess)
             {
-                Assert.That(response.Result.Choices[0].Text.Contains("Say this is a test"), Is.EqualTo(echo), "Prompt not returned when Echo was true");
+                var containsPrompt = response.Result.Choices[0].Text.Contains("Say this is a test");
+                Assert.IsTrue(containsPrompt, "Prompt not returned when Echo was true");
             }
             else if(isSuccess)
             {
@@ -62,6 +63,31 @@ namespace OpenAI.Net.Integration.Tests
             {
                 Assert.That(request.Echo, Is.EqualTo(null), "Echo default should be null/not set");
             }
+        }
+
+
+        [TestCase("text-davinci-003", true, HttpStatusCode.OK, null, TestName = "GetList_When_Success")]
+        [TestCase("invalid_model", false, HttpStatusCode.NotFound, false, TestName = "GetList_When_Fail")]
+        public async Task GetListExtension(string model, bool isSuccess, HttpStatusCode statusCode, bool? echo)
+        {
+            var request = new TextCompletionRequest(model, "Say this is a test");
+
+            var prompts = new List<string>()
+            {
+                "Say this is a test",
+                "Say this is not a test"
+            };
+
+            var response = await OpenAIService.TextCompletion.Get(model, prompts, (o) => {
+                o.MaxTokens = 1024;
+                o.BestOf = 2;
+            });
+
+
+            Assert.That(response.IsSuccess, Is.EqualTo(isSuccess), "Request failed");
+            Assert.That(response.StatusCode, Is.EqualTo(statusCode));
+            Assert.That(response?.Result?.Choices?.Count() == 2, Is.EqualTo(isSuccess), "Choices are not mapped correctly");
+            //NB Note thatn when using array as input, echo does not seem to work.
         }
     }
 }
