ee8d855d4e0df9222e58204e45331bb88f8d408a
Justim Odendaal <justim.odendaal@gmail.com>
2023-03-25 06:39:22 +0000

    Updated Model TextDavinciEdit001 looks to have been dropped from Open AI.

--- src/OpenAI.Net.Integration.Tests/ModelsService.cs
+++ src/OpenAI.Net.Integration.Tests/ModelsService.cs
@@ -1,10 +1,11 @@
-﻿using System.Net;
+﻿using Newtonsoft.Json;
+using System.Net;
 
 namespace OpenAI.Net.Integration.Tests
 {
     internal class ModelsService : BaseTest
     {
-        [TestCase(ModelTypes.TextDavinciEdit001,true, HttpStatusCode.OK,TestName = "GetById_When_Success")]
+        [TestCase(ModelTypes.GPT35Turbo,true, HttpStatusCode.OK,TestName = "GetById_When_Success")]
         [TestCase("invalid_model", false, HttpStatusCode.NotFound,TestName = "GetById_When_Invalid_Model_Fail")]
         public async Task GetById(string model,bool isSuccess, HttpStatusCode statusCode)
         {
@@ -14,7 +15,7 @@ namespace OpenAI.Net.Integration.Tests
 
             Assert.That(response.IsSuccess, Is.EqualTo(isSuccess), "Request failed");
             Assert.That(response.StatusCode, Is.EqualTo(statusCode));
-            Assert.That(response.Result?.Id == ModelTypes.TextDavinciEdit001, Is.EqualTo(isSuccess), "Choices are not mapped correctly");
+            Assert.That(response.Result?.Id == ModelTypes.GPT35Turbo, Is.EqualTo(isSuccess), "Choices are not mapped correctly");
         }
 
         [TestCase(true, HttpStatusCode.OK)]
@@ -23,7 +24,7 @@ namespace OpenAI.Net.Integration.Tests
             
 
             var response = await OpenAIService.Models.Get();
-
+            Console.WriteLine(JsonConvert.SerializeObject(response.Result.Data));
             Assert.That(response.IsSuccess, Is.EqualTo(isSuccess), "Request failed");
             Assert.That(response.StatusCode, Is.EqualTo(statusCode));
             Assert.That(response.Result?.Data.Count() > 0, Is.EqualTo(isSuccess), "Choices are not mapped correctly");
