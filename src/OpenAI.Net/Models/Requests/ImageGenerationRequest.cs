98ef38fb177f27dcf7869dc3453826c2dcb98581
Justim Odendaal <justim.odendaal@gmail.com>
2023-11-10 10:13:11 +0000

    Update doc strings

--- src/OpenAI.Net/Models/Requests/ImageGenerationRequest.cs
+++ src/OpenAI.Net/Models/Requests/ImageGenerationRequest.cs
@@ -11,8 +11,9 @@ namespace OpenAI.Net.Models.Requests
         }
 
         /// <summary>
-        /// A text description of the desired image(s). The maximum length is 1000 characters. <br/>
-        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images/create-prompt" />
+        /// A text description of the desired image(s). The maximum length is 1000 characters <br />
+        /// for dall-e-2 and 4000 characters for dall-e-3. <br/>
+        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-prompt" />
         /// </summary>
         [Required]
         public string Prompt { get; set; }
@@ -20,20 +21,20 @@ namespace OpenAI.Net.Models.Requests
         /// <summary>
         /// The number of images to generate. Must be between 1 and 10. <br/>
         /// Defaults to 1 <br/>
-        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images/create-n" />
+        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-n" />
         /// </summary>
         public int? N { get; set; }
 
         /// <summary>
-        /// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024. <br/>
-        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images/create-size" />
-        /// </summary>
+        /// The size of the generated images. Must be one of 256x256, 512x512, or 1024x1024 <br />
+        /// for dall-e-2. Must be one of 1024x1024, 1792x1024, or 1024x1792 for dall-e-3 models. <br/>
+        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-size" />
         public string Size { get; set; }
 
         /// <summary>
         /// The format in which the generated images are returned. Must be one of url or b64_json. <br/>
         /// Defaults to url <br/>
-        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images/create-response_format" />
+        /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-response_format" />
         /// </summary>
         [JsonPropertyName("response_format")]
         public string ResponseFormat { get; set; }
@@ -46,15 +47,16 @@ namespace OpenAI.Net.Models.Requests
         public string User { get; set; }
 
         /// <summary>
-        /// The model to use for image generation.Defaults to dall-e-2 <br/>
-        /// <a href="https://platform.openai.com/docs/api-reference/images/create#images-create-model">Learn more</a>. <br/>
+        /// The model to use for image generation. <br/>
+        /// Defaults to dall-e-2 <br/>
         /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-model" />
         /// </summary>
         public string? Model { get; set; }
 
-        /// <summary>
-        /// The quality of the image that will be generated. hd creates images with finer details and greater consistency across the image. This param is only supported for dall-e-3.Defaults to standard <br/>
-        /// <a href="https://platform.openai.com/docs/api-reference/images/create#images-create-quality">Learn more</a>. <br/>
+        //// <summary>
+        /// The quality of the image that will be generated. hd creates images with finer details <br />
+        /// and greater consistency across the image. This param is only supported for dall-e-3. <br/>
+        /// Defaults to standard <br/>
         /// <see href="https://platform.openai.com/docs/api-reference/images/create#images-create-quality" />
         /// </summary>
         public string? Quality { get; set; }
